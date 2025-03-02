470007d9a4c5611aaee5bdc6a1102db710297b50
Michael Becker <mdbecker@users.noreply.github.com>
2025-03-02 15:21:10 -0500

    chore(start): remove debug logging from start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -e
 
-# === DEBUGGING OUTPUT ===
-echo "DEBUG: Running start.sh" 1>&2
-echo "DEBUG: PATH=$PATH" 1>&2
-echo "DEBUG: PYTHON=$(which python)" 1>&2
-echo "DEBUG: PYTHON_VERSION=$(python --version 2>&1)" 1>&2
-echo "DEBUG: JUPYTER=$(which jupyter)" 1>&2
-echo "DEBUG: JUPYTER_VERSION=$(jupyter --version 2>&1)" 1>&2
-
 exec "${HOME}/pixi-activate.sh" jupyter lab \
   --no-browser \
   --ip=0.0.0.0 \
